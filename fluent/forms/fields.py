1db3d00309fbda2d1bc40b917b04e433b1fb431c
Luke Benstead <kazade@gmail.com>
2016-01-28 13:29:27 +0000

    Improve the TranslatableField form field

--- fluent/forms/fields.py
+++ fluent/forms/fields.py
@@ -1,14 +1,24 @@
 from django import forms
+from django.conf import settings
+
 from fluent.fields import TranslatableContent
 
 
 class TranslatableField(object):
+    def __init__(self, language_code=None, hint=u"", *args, **kwargs):
+        self.language_code = language_code or settings.LANGUAGE_CODE
+        self.hint = hint
+
     def clean(self, value):
         value = super(TranslatableField, self).clean(value)
         if value:
             return TranslatableContent(
-                text=value
+                text=value,
+                hint=self.hint,
+                language_code=self.language_code
             )
+        else:
+            return TranslatableContent(hint=self.hint, language_code=self.language_code)
 
 
 class TranslatableCharField(TranslatableField, forms.CharField):
