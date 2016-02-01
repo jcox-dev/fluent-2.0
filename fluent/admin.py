81b73869dcefd3d93b82e309fb377d27b4502f87
Luke Benstead <kazade@gmail.com>
2016-02-01 15:11:55 +0000

    Update the admin to include a new 'scan' page

--- fluent/admin.py
+++ fluent/admin.py
@@ -1,8 +1,31 @@
 from django.contrib import admin
+from django.shortcuts import render
+from django.conf.urls import patterns, url
 
 from fluent.models import MasterTranslation, Translation
 
 
+def scan_view(request):
+    subs = {}
 
-admin.site.register(MasterTranslation)
+    if request.method == "POST":
+        pass
+    else:
+        pass
+
+    return render(request, "fluent/scan.html", subs)
+
+
+class MasterTranslationAdmin(admin.ModelAdmin):
+
+    def get_urls(self):
+        urls = super(MasterTranslationAdmin, self).get_urls()
+
+        urls = patterns('',
+            url(r'scan/$', scan_view, name="fluent_translation_scan"),
+        ) + urls
+
+        return urls
+
+admin.site.register(MasterTranslation, MasterTranslationAdmin)
 admin.site.register(Translation)
