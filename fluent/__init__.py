041230e0c898e371a51c54ad3331ac877b641f37
Luke Benstead <kazade@gmail.com>
2016-02-01 10:51:15 +0000

    Add a stub AppConfig and model_mommy generator for translatable content

---
+++ fluent/__init__.py
@@ -0,0 +1,2 @@
+
+default_app_config = 'fluent.apps.FluentAppConfig'
---
+++ fluent/apps.py
@@ -0,0 +1,23 @@
+
+
+try:
+    # Configure a generator if the user is using model_mommy
+
+    from model_mommy import generators
+
+    def gen_translatablecontent(max_length):
+        from fluent.fields import TranslatableContent
+        return TranslatableContent(text=generators.gen_string(max_length))
+    gen_translatablecontent.required = ['max_length']
+
+    MOMMY_CUSTOM_FIELDS_GEN = {
+        'fluent.fields.TranslatableField': gen_translatablecontent,
+    }
+except ImportError:
+    pass
+
+
+from django.apps import AppConfig
+
+class FluentAppConfig(AppConfig):
+    pass
