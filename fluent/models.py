4fdb606c4f6d673d1601760fed5ee24fdb9b0796
Matt Molyneaux <mattam@potatolondon.com>
2016-03-04 13:17:10 +0000

    Add __unicode__ method to Translation model
    Makes the admin view slightly more useful!
    
    Also added tests for __unicode__ on both Translation and
    MasterTranslation

--- fluent/models.py
+++ fluent/models.py
@@ -64,6 +64,12 @@ class Translation(models.Model):
         if msgs:
             raise ValidationError([err for err, _orig, _trans in msgs])
 
+    def __unicode__(self):
+        single_form_key = get_plural_index(self.language_code, 1)
+        plural_keys = self.plural_texts.keys()
+        plural_keys.remove(single_form_key)
+        return u"{} ({}{})".format(self.text, self.language_code, ' plural' if len(plural_keys) > 0 else '')
+
     @staticmethod
     def generate_hash(master_text, master_hint):
         assert master_text
