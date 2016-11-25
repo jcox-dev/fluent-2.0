3d46f8ce14686af6463f30c26e63bd3f270504d1
Ola Sendecka <asendecka@google.com>
2016-11-25 15:47:59 +0000

    Split tests into smaller more atomic tests.
    * we test each scenario separately, also with and without
    MasterTranslation
    * I left the previous test in place without variable blocktrans to
    have an integration test with mulitple tags inside as well

--- fluent/tests/test_scanner.py
+++ fluent/tests/test_scanner.py
@@ -2,6 +2,7 @@ from djangae.test import TestCase
 from django.template import Template, Context
 
 from fluent.scanner import parse_file, DEFAULT_TRANSLATION_GROUP
+from fluent.models import MasterTranslation
 
 
 TEST_HTML_CONTENT = """{% load fluent %}
@@ -21,18 +22,6 @@ Test blocktrans & escaping
 {% endblocktrans %}
 {% blocktrans noescape %}
 Test blocktrans & unescaping
-{% endblocktrans %}
-{% blocktrans %}
-<a href="http://google.com">{{ name }}</a> without group
-{% endblocktrans %}
-{% blocktrans noescape %}
-<a href="http://google.com">{{ name }}</a> without group
-{% endblocktrans %}
-{% blocktrans group "public" %}
-<a href="http://google.com">{{ name }}</a> in group
-{% endblocktrans %}
-{% blocktrans noescape group "public" %}
-<a href="http://google.com">{{ name }}</a> in group
 {% endblocktrans %}
 """
 
@@ -54,16 +43,302 @@ class ScannerTests(TestCase):
             ('Test trans string with group', '', '', 'public'),
             ('Test trans string without group', '', '', DEFAULT_TRANSLATION_GROUP),
             ('Test & escaping', '', '', DEFAULT_TRANSLATION_GROUP),
+            ('Test & unescaping', '', '', DEFAULT_TRANSLATION_GROUP),
             ('\nTest trans block with group\n', '', '', 'public'),
             ('\nTest trans block without group\n', '', '', DEFAULT_TRANSLATION_GROUP),
             ('\nTest blocktrans & escaping\n', '', '', DEFAULT_TRANSLATION_GROUP),
             ('\nTest blocktrans & unescaping\n', '', '', DEFAULT_TRANSLATION_GROUP),
-            ('\n<a href="http://google.com">%(name)s</a> without group\n', '', '', DEFAULT_TRANSLATION_GROUP),
-            ('\n<a href="http://google.com">%(name)s</a> without group\n', '', '', DEFAULT_TRANSLATION_GROUP),
-            ('\n<a href="http://google.com">%(name)s</a> in group\n', '', '', 'public'),
-            ('\n<a href="http://google.com">%(name)s</a> in group\n', '', '', 'public'),
         ]
         self.assertEqual(results, expected)
+
+    def test_trans_tag_with_group(self):
+        text = "Test trans string with group"
+        content = """
+{% load fluent %}
+{% trans "Test trans string with group" group "public" %}
+        """
+        results = parse_file(content, ".html")
+        expected = [
+            (text, '', '', 'public'),
+        ]
+        self.assertEqual(results, expected)
+        rendered = Template(content).render(Context())
+        self.assertTrue("Test trans string with group" in rendered)
+
+        # create master translation for the string and test it renders correctly
+        key = MasterTranslation.generate_key(text, '', 'en-us')
+        mt = MasterTranslation.objects.create(
+            pk=key, text=text, language_code='en-us'
+        )
+        mt.used_by_groups_in_code_or_templates = {'public'}
+        mt.save()
+
+        rendered = Template(content).render(Context())
+        self.assertTrue("Test trans string with group" in rendered)
+
+    def test_trans_tag_without_group(self):
+        text = "Test trans string without group"
+        content = """
+{% load fluent %}
+{% trans "Test trans string without group" %}
+        """
+        results = parse_file(content, ".html")
+        expected = [(text, '', '', DEFAULT_TRANSLATION_GROUP)]
+        self.assertEqual(results, expected)
+
+        rendered = Template(content).render(Context())
+        self.assertTrue(text in rendered)
+
+        # create master translation for the string and test it renders correctly
+        key = MasterTranslation.generate_key(text, '', 'en-us')
+        MasterTranslation.objects.create(
+            pk=key, text=text, language_code='en-us'
+        )
+        rendered = Template(content).render(Context())
+        self.assertTrue("Test trans string without group" in rendered)
+
+    def test_trans_tag_and_escaping(self):
+        text = "Test & escaping"
+        content = """
+{% load fluent %}
+{% trans "Test & escaping" %}
+        """
+        results = parse_file(content, ".html")
+        expected = [
+            (text, '', '', DEFAULT_TRANSLATION_GROUP),
+        ]
+        self.assertEqual(results, expected)
+        rendered = Template(content).render(Context())
+        self.assertTrue("Test &amp; escaping" in rendered)
+
+        # create master translation for the string and test it renders correctly
+        key = MasterTranslation.generate_key(text, '', 'en-us')
+        MasterTranslation.objects.create(
+            pk=key, text=text, language_code='en-us'
+        )
+        rendered = Template(content).render(Context())
+        self.assertTrue("Test &amp; escaping" in rendered)
+
+    def test_trans_tag_and_noescape(self):
+        text = "Test & unescaping"
+        content = """
+{% load fluent %}
+{% trans "Test & unescaping" noescape %}
+        """
+        results = parse_file(content, ".html")
+        expected = [(text, '', '', DEFAULT_TRANSLATION_GROUP)]
+        self.assertEqual(results, expected)
+
+        rendered = Template(content).render(Context())
+        self.assertTrue(text in rendered)
+
+        # create master translation for the string and test it renders correctly
+        key = MasterTranslation.generate_key(text, '', 'en-us')
+        MasterTranslation.objects.create(
+            pk=key, text=text, language_code='en-us'
+        )
+        rendered = Template(content).render(Context())
+        self.assertTrue("Test & unescaping" in rendered)
+
+    def test_blocktrans_tag_with_group(self):
+        text = "\nTest trans block with group\n"
+        content = """
+{% load fluent %}
+{% blocktrans group "public" %}
+Test trans block with group
+{% endblocktrans %}
+        """
+        results = parse_file(content, ".html")
+        expected = [
+            (text, '', '', 'public'),
+        ]
+        self.assertEqual(results, expected)
+        rendered = Template(content).render(Context())
+        self.assertTrue(text in rendered)
+
+        # create master translation for the string and test it renders correctly
+        key = MasterTranslation.generate_key(text, '', 'en-us')
+        mt = MasterTranslation.objects.create(
+            pk=key, text=text, language_code='en-us'
+        )
+        mt.used_by_groups_in_code_or_templates = {'public'}
+        mt.save()
+
+        rendered = Template(content).render(Context())
+        self.assertTrue("Test trans block with group" in rendered)
+
+    def test_blocktrans_tag_without_group(self):
+        text = "\nTest trans block without group\n"
+        content = """
+{% load fluent %}
+{% blocktrans %}
+Test trans block without group
+{% endblocktrans %}
+        """
+        results = parse_file(content, ".html")
+        expected = [(text, '', '', DEFAULT_TRANSLATION_GROUP)]
+        self.assertEqual(results, expected)
+
+        rendered = Template(content).render(Context())
+        self.assertTrue(text in rendered)
+
+        # create master translation for the string and test it renders correctly
+        key = MasterTranslation.generate_key(text, '', 'en-us')
+        MasterTranslation.objects.create(
+            pk=key, text=text, language_code='en-us'
+        )
+        rendered = Template(content).render(Context())
+        self.assertTrue("Test trans block without group" in rendered)
+
+    def test_blocktrans_tag_and_escaping(self):
+        text = "\nTest blocktrans & escaping\n"
+        content = """
+{% load fluent %}
+{% blocktrans %}
+Test blocktrans & escaping
+{% endblocktrans %}
+        """
+        results = parse_file(content, ".html")
+        expected = [
+            (text, '', '', DEFAULT_TRANSLATION_GROUP),
+        ]
+        self.assertEqual(results, expected)
+        rendered = Template(content).render(Context())
+        self.assertTrue("Test blocktrans &amp; escaping" in rendered)
+
+        # create master translation for the string and test it renders correctly
+        key = MasterTranslation.generate_key(text, '', 'en-us')
+        MasterTranslation.objects.create(
+            pk=key, text=text, language_code='en-us'
+        )
+        rendered = Template(content).render(Context())
+        self.assertTrue("Test blocktrans &amp; escaping" in rendered)
+
+    def test_blocktrans_tag_and_noescape(self):
+        text = "\nTest blocktrans & unescaping\n"
+        content = """
+{% load fluent %}
+{% blocktrans noescape %}
+Test blocktrans & unescaping
+{% endblocktrans %}
+        """
+        results = parse_file(content, ".html")
+        expected = [(text, '', '', DEFAULT_TRANSLATION_GROUP)]
+        self.assertEqual(results, expected)
+
+        rendered = Template(content).render(Context())
+        self.assertTrue(text in rendered)
+
+        # create master translation for the string and test it renders correctly
+        key = MasterTranslation.generate_key(text, '', 'en-us')
+        MasterTranslation.objects.create(
+            pk=key, text=text, language_code='en-us'
+        )
+        rendered = Template(content).render(Context())
+        self.assertTrue("Test blocktrans & unescaping" in rendered)
+
+    def test_blocktrans_tag_with_variable_and_group_escaping(self):
+        text = '\n<a href="http://google.com">%(name)s</a> in group\n'
+        content = """
+{% load fluent %}
+{% blocktrans group "public" %}
+<a href="http://google.com">{{ name }}</a> in group
+{% endblocktrans %}
+        """
+        results = parse_file(content, ".html")
+        expected = [
+            (text, '', '', 'public'),
+        ]
+        self.assertEqual(results, expected)
+
+        data = {'name': "Ola & Ola"}
+        rendered = Template(content).render(Context(data))
+        self.assertTrue('&lt;a href=&quot;http://google.com&quot;&gt;Ola &amp; Ola&lt;/a&gt; in group' in rendered)
+
+        # create master translation for the string and test it renders correctly
+        key = MasterTranslation.generate_key(text, '', 'en-us')
+        mt = MasterTranslation.objects.create(
+            pk=key, text=text, language_code='en-us'
+        )
+        mt.used_by_groups_in_code_or_templates = {'public'}
+        mt.save()
+
+        rendered = Template(content).render(Context(data))
+        self.assertTrue('&lt;a href=&quot;http://google.com&quot;&gt;Ola &amp; Ola&lt;/a&gt; in group' in rendered)
+
+    def test_blocktrans_tag_with_variable_and_with_group_noescaping(self):
+        text = '\n<a href="http://google.com">%(name)s</a> in group\n'
+        content = """
+{% load fluent %}
+{% blocktrans noescape group "public" %}
+<a href="http://google.com">{{ name }}</a> in group
+{% endblocktrans %}
+        """
+        results = parse_file(content, ".html")
+        expected = [(text, '', '', 'public')]
+        self.assertEqual(results, expected)
+
+        data = {'name': "Ola & Ola"}
+        rendered = Template(content).render(Context(data))
+        self.assertTrue('<a href="http://google.com">Ola &amp; Ola</a> in group' in rendered)
+
+        # create master translation for the string and test it renders correctly
+        key = MasterTranslation.generate_key(text, '', 'en-us')
+        MasterTranslation.objects.create(
+            pk=key, text=text, language_code='en-us'
+        )
+        rendered = Template(content).render(Context(data))
+        self.assertTrue('<a href="http://google.com">Ola &amp; Ola</a> in group' in rendered)
+
+    def test_blocktrans_tag_with_variable_escaping(self):
+        text = '\n<a href="http://google.com">%(name)s</a> without group\n'
+        content = """
+{% load fluent %}
+{% blocktrans %}
+<a href="http://google.com">{{ name }}</a> without group
+{% endblocktrans %}
+        """
+        results = parse_file(content, ".html")
+        expected = [
+            (text, '', '', DEFAULT_TRANSLATION_GROUP),
+        ]
+        self.assertEqual(results, expected)
+
+        data = {'name': "Ola & Ola"}
+        rendered = Template(content).render(Context(data))
+        self.assertTrue('&lt;a href=&quot;http://google.com&quot;&gt;Ola &amp; Ola&lt;/a&gt; without group' in rendered)
+
+        # create master translation for the string and test it renders correctly
+        key = MasterTranslation.generate_key(text, '', 'en-us')
+        MasterTranslation.objects.create(
+            pk=key, text=text, language_code='en-us'
+        )
+        rendered = Template(content).render(Context(data))
+        self.assertTrue('&lt;a href=&quot;http://google.com&quot;&gt;Ola &amp; Ola&lt;/a&gt; without group' in rendered)
+
+    def test_blocktrans_tag_with_variable_and_noescaping(self):
+        text = '\n<a href="http://google.com">%(name)s</a> without group\n'
+        content = """
+{% load fluent %}
+{% blocktrans noescape %}
+<a href="http://google.com">{{ name }}</a> without group
+{% endblocktrans %}
+        """
+        results = parse_file(content, ".html")
+        expected = [(text, '', '', DEFAULT_TRANSLATION_GROUP)]
+        self.assertEqual(results, expected)
+
+        data = {'name': "Ola & Ola"}
+        rendered = Template(content).render(Context(data))
+        self.assertTrue('<a href="http://google.com">Ola &amp; Ola</a> without group' in rendered)
+
+        # create master translation for the string and test it renders correctly
+        key = MasterTranslation.generate_key(text, '', 'en-us')
+        MasterTranslation.objects.create(
+            pk=key, text=text, language_code='en-us'
+        )
+        rendered = Template(content).render(Context(data))
+        self.assertTrue('<a href="http://google.com">Ola &amp; Ola</a> without group' in rendered)
 
     def test_basic_python_parsing(self):
         results = parse_file(TEST_PYTHON_CONTENT, ".py")
@@ -83,11 +358,3 @@ class ScannerTests(TestCase):
         self.assertTrue("Test trans block with group" in rendered)
         self.assertTrue("Test blocktrans &amp; escaping" in rendered)
         self.assertTrue("Test blocktrans & unescaping" in rendered)
-
-        self.assertTrue('<a href="http://google.com">Ola &amp; Ola</a> in group' in rendered)
-        # even if blocktrans has "noescape" the & from variable is escaped
-        self.assertFalse('<a href="http://google.com">Ola & Ola</a> in group' in rendered)
-
-        self.assertTrue('<a href="http://google.com">Ola &amp; Ola</a> without group' in rendered)
-        # even if blocktrans has "noescape" the & from variable is escaped
-        self.assertFalse('<a href="http://google.com">Ola & Ola</a> without group' in rendered)
