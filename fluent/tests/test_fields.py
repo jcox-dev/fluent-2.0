af94d3db9cf9fcb9c6be59de3d4f46a1cc43b3e1
Grzes Furga <grzes@potatolondon.com>
2016-12-12 16:45:57 +0100

    Avoid using Test in model names because nose get's confused.

--- fluent/tests/test_fields.py
+++ fluent/tests/test_fields.py
@@ -17,7 +17,7 @@ from fluent.models import MasterTranslation
 from fluent.patches import monkey_patch
 
 
-class TestModel(models.Model):
+class TranslatedModel(models.Model):
     class Meta:
         app_label = "fluent"
 
@@ -28,7 +28,7 @@ class TestModel(models.Model):
     trans_with_default = TranslatableCharField(blank=True, default=TranslatableContent(text="Adirondack"))
 
 
-class TestBadDefaultModel(models.Model):
+class BadDefaultModel(models.Model):
     class Meta:
         # don't get counted in the locating test
         app_label = "fluent_test"
@@ -39,7 +39,7 @@ class TestBadDefaultModel(models.Model):
 class TranslatableCharFieldTests(TestCase):
 
     def test_unset_translations(self):
-        m = TestModel.objects.create()
+        m = TranslatedModel.objects.create()
 
         self.assertEqual("", m.trans.text)
         self.assertEqual("", m.trans_with_hint.text)
@@ -57,23 +57,23 @@ class TranslatableCharFieldTests(TestCase):
         self.assertEqual(m.trans_with_default.text_for_language_code("en"), "Adirondack")
 
     def test_setting_and_getting_translation_text(self):
-        m = TestModel()
+        m = TranslatedModel()
         m.trans.text = "Hello World!"
         m.trans_with_group.text = "Hello World!"
         m.trans_with_hint.text = "Hello World!"
         m.save()
 
         # get a completely fresh object, no internal caches etc.
-        m = TestModel.objects.get()
+        m = TranslatedModel.objects.get()
         text = m.trans.text_for_language_code("en")
         self.assertEqual(text, "Hello World!")
 
     def test_finding_all_translations_for_a_group(self):
-        TestModel.objects.create()
+        TranslatedModel.objects.create()
         translations = MasterTranslation.find_by_group("Test")
         self.assertEqual(0, translations.count())
 
-        TestModel.objects.create(trans_with_group=TranslatableContent(text="Hello World!"))
+        TranslatedModel.objects.create(trans_with_group=TranslatableContent(text="Hello World!"))
 
         translations = MasterTranslation.find_by_group("Test")
         self.assertEqual(1, translations.count())
@@ -83,20 +83,20 @@ class TranslatableCharFieldTests(TestCase):
         A non-TranslatableContent default value should raise a ValueError
         """
         with self.assertRaises(ValueError):
-            TestBadDefaultModel.objects.create()
+            BadDefaultModel.objects.create()
 
     def test_with_model_mommy(self):
         monkey_patch()  # Enable custom generator
 
-        obj = mommy.make(TestModel)
+        obj = mommy.make(TranslatedModel)
         self.assertTrue(obj.trans.text)
 
     def test_delete_does_nothing(self):
         """ Test that deleting a MasterTranslation does not delete the model which uses it. """
-        TestModel.objects.create(trans=TranslatableContent("whatever"))
-        self.assertEqual(TestModel.objects.count(), 1)
+        TranslatedModel.objects.create(trans=TranslatableContent("whatever"))
+        self.assertEqual(TranslatedModel.objects.count(), 1)
         MasterTranslation.objects.all().delete()
-        self.assertEqual(TestModel.objects.count(), 1)
+        self.assertEqual(TranslatedModel.objects.count(), 1)
 
 
 class TestLocatingTranslatableFields(TestCase):
@@ -106,12 +106,12 @@ class TestLocatingTranslatableFields(TestCase):
         # Just filter the results down to this app
         results = [ x for x in results if x[0]._meta.app_label == "fluent" ]
 
-        # Should return the 4 fields of TestModel above
+        # Should return the 4 fields of TranslatedModel above
         self.assertEqual(4, len(results))
-        self.assertEqual(TestModel, results[0][0])
-        self.assertEqual(TestModel, results[1][0])
-        self.assertEqual(TestModel, results[2][0])
-        self.assertEqual(TestModel, results[3][0])
+        self.assertEqual(TranslatedModel, results[0][0])
+        self.assertEqual(TranslatedModel, results[1][0])
+        self.assertEqual(TranslatedModel, results[2][0])
+        self.assertEqual(TranslatedModel, results[3][0])
 
         results = find_all_translatable_fields(with_group="Test")
         # Just filter the results down to this app
@@ -119,7 +119,7 @@ class TestLocatingTranslatableFields(TestCase):
 
         # Should return the one field with this group
         self.assertEqual(1, len(results))
-        self.assertEqual(TestModel, results[0][0])
+        self.assertEqual(TranslatedModel, results[0][0])
 
     def test_find_installed_translatable_fields(self):
         results = find_installed_translatable_fields()
@@ -130,12 +130,12 @@ class TestLocatingTranslatableFields(TestCase):
             if model._meta.app_label == 'fluent':
                 fluent_app_translatable_fields.extend(results[model])
 
-        # Should return the 4 fields of TestModel above
+        # Should return the 4 fields of TranslatedModel above
         self.assertEqual(4, len(fluent_app_translatable_fields))
-        self.assertEqual(TestModel, fluent_app_translatable_fields[0].model)
-        self.assertEqual(TestModel, fluent_app_translatable_fields[1].model)
-        self.assertEqual(TestModel, fluent_app_translatable_fields[2].model)
-        self.assertEqual(TestModel, fluent_app_translatable_fields[3].model)
+        self.assertEqual(TranslatedModel, fluent_app_translatable_fields[0].model)
+        self.assertEqual(TranslatedModel, fluent_app_translatable_fields[1].model)
+        self.assertEqual(TranslatedModel, fluent_app_translatable_fields[2].model)
+        self.assertEqual(TranslatedModel, fluent_app_translatable_fields[3].model)
 
         results = find_installed_translatable_fields(with_groups=["Test"])
         fluent_app_translatable_fields = []
@@ -145,7 +145,7 @@ class TestLocatingTranslatableFields(TestCase):
 
         # Should return the one field with this group
         self.assertEqual(1, len(fluent_app_translatable_fields))
-        self.assertEqual(TestModel, fluent_app_translatable_fields[0].model)
+        self.assertEqual(TranslatedModel, fluent_app_translatable_fields[0].model)
 
 
 @override_settings(LANGUAGES=[("en", "English"), ("de", "German")])
